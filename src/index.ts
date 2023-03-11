7cee66bb3bd276abf2868c6bb0e04136155c19cb
Hieu Pham <phaminhieu.dev@gmail.com>
2023-03-10 23:47:12 -0800

    add scene view

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-import Map from "@arcgis/core/Map.js";
-import MapView from "@arcgis/core/views/MapView.js"
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView"
+import SceneView from "@arcgis/core/views/SceneView"
 import esriConfig from "@arcgis/core/config";
 import { key } from "../api-key.json";
 
 esriConfig.apiKey = key;
 
+/* Maps and scenes */
+/* --------------- */
+/* --------------- */
+/* --------------- */
+
+/* Display a map */
+// const map = new Map({
+//   basemap: "arcgis-topographic"
+// });
+
+
+// const view = new MapView({
+//   map,
+//   center: [-118.805, 34.027], // longtitie, latitude
+//   zoom: 13,
+//   container: "viewDiv"
+// });
+
+/* Display a scene (3D) */
 const map = new Map({
-  basemap: "arcgis-topographic"
+  basemap: "arcgis-topographic",
+  ground: "world-elevation"
 });
 
-
-const view = new MapView({
+const sceneView = new SceneView({
   map,
-  center: [-118.805, 34.027],
-  zoom: 13,
-  container: "viewDiv"
+  container: "viewDiv",
+  camera: {
+    position: {
+      x: -118.808, // longtitude
+      y: 33.961, //latitude
+      z: 2000 //memters
+    },
+    tilt: 75
+  }
 });
