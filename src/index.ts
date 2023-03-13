b69bf695195da6890f29df476e7e85856ff9311e
Hieu Pham <phaminhieu.dev@gmail.com>
2023-03-12 18:07:19 -0700

    add custom basemap

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import MapView from "@arcgis/core/views/MapView";
 import SceneView from "@arcgis/core/views/SceneView";
 import BasemapToggle from "@arcgis/core/widgets/BasemapToggle";
 import BasemapGallery from "@arcgis/core/widgets/BasemapGallery";
+import Basemap from "@arcgis/core/Basemap";
+import VectorTileLayer from "@arcgis/core/layers/VectorTileLayer";
+import TileLayer from "@arcgis/core/layers/TileLayer";
 import esriConfig from "@arcgis/core/config";
 import { key } from "../api-key.json";
 
@@ -14,9 +17,9 @@ esriConfig.apiKey = key;
 /* --------------- */
 
 /* Display a map */
-const map = new Map({
-  basemap: "arcgis-topographic"
-});
+// const map = new Map({
+//   basemap: "arcgis-topographic"
+// });
 
 
 // const view = new MapView({
@@ -47,27 +50,61 @@ const map = new Map({
 
 
 /* Change the Basemap layer */
-const view = new MapView({
-  container: "viewDiv",
-  map,
-  center: [-118.80543, 34.02700],
-  zoom: 13
+// const view = new MapView({
+//   container: "viewDiv",
+//   map,
+//   center: [-118.80543, 34.02700],
+//   zoom: 13
+// });
+
+// const basemapToggle = new BasemapToggle({
+//   view,
+//   nextBasemap: "arcgis-imagery"
+// });
+
+// view.ui.add(basemapToggle, "bottom-right");
+
+// const basemapGallery = new BasemapGallery({
+//   view,
+//   source: {
+//     query: {
+//       title: '"World Basemaps for Developers" AND owner:esri',
+//     }
+//   }
+// });
+
+// view.ui.add(basemapGallery, "top-right");
+
+
+/* Display a custom basemap style */
+const vectorTileLayer = new VectorTileLayer({
+  portalItem: {
+    id: "d2ff12395aeb45998c1b154e25d680e5" // Forest and Parks Canvas
+  },
+  opacity: .75
 });
 
-const basemapToggle = new BasemapToggle({
-  view,
-  nextBasemap: "arcgis-imagery"
-});
-
-view.ui.add(basemapToggle, "bottom-right");
-
-const basemapGallery = new BasemapGallery({
-  view,
-  source: {
-    query: {
-      title: '"World Basemaps for Developers" AND owner:esri',
-    }
+const imageTileLayer = new TileLayer({
+  portalItem: {
+    id: "1b243539f4514b6ba35e7d995890db1d" // World Hillshade
   }
 });
 
-view.ui.add(basemapGallery, "top-right");
+const baseMap = new Basemap({
+  baseLayers: [
+    imageTileLayer,
+    vectorTileLayer
+  ]
+});
+
+const map = new Map({
+  basemap: baseMap
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map,
+
+  center: [-100,40],
+  zoom: 3
+});
